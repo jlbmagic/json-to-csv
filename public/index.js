ff71f009e364eb3e519935056d794001d87a4847
Jeremy Brown <jeremy@geistinteractive.com>
2021-07-29 17:53:39 -0400

    first dev

--- public/index.js
+++ public/index.js
@@ -2,10 +2,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import "regenerator-runtime/runtime.js";
 
+const json2csv = require("csvjson-json2csv");
 
-const setText = () => {
-  document.getElementById("text").innerHTML =
-    "We are beginning! Oh Yes, we are!";
+window.getAndProcessJSON = (json, params) => {
+  // alert("Get");
+
+  const csv = json2csv(json, JSON.parse(params));
+  const obj = btoa(csv);
+  FileMaker.PerformScript("Return Data", obj);
 };
-
-document.getElementById("click").addEventListener("click", setText);
